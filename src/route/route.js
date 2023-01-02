9f1615ef81c1597a3b3cff9636d7e85afd300ce9
GauravDattuPise <GauravDattuPise>
2023-01-02 16:35:11 +0530

    blogs created

--- src/route/route.js
+++ src/route/route.js
@@ -1,11 +1,14 @@
 const express=require("express");
 const router=express.Router();
 const authorController=require("../controller/authorController")
+const blogController = require("../controller/blogController")
 
 //create author
 router.post("/authors",authorController.createAuthor)
 
+// CREATING BLOGS
 
+router.post("/blogs",blogController.createBlog)
 
 
 
