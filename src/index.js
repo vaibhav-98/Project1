d9e8fea94a568d44a66533f3bfa081a93cfb53fc
gabhishekrao <g.abhishek.raoo@gmail.com>
2023-01-02 20:25:26 +0530

    add

--- src/index.js
+++ src/index.js
@@ -7,13 +7,8 @@ const app = express()
 
 app.use(express.json())
 
-<<<<<<< HEAD
-mongoose.connect("mongodb+srv://gabhishek:GAbhishekPassword@gabhishek01.dq23z80.mongodb.net/miniBlogs", {
-      useNewUrlParser: true
-=======
 mongoose.connect("mongodb+srv://gabhishek:GAbhishekPassword@gabhishek01.dq23z80.mongodb.net/miniBlogs",{
       useNewUrlParser:true
->>>>>>> 9f1615ef81c1597a3b3cff9636d7e85afd300ce9
 })
       .then(() => console.error("My DB is connected"))
       .catch((err) => console.error(err))
