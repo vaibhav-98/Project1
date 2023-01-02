84fe658b8301a6abca19e839cbc8ae0b408f5603
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-01-02 21:15:32 +0530

    blogcreation updated1

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -1,17 +1,29 @@
+const mongoose = require("mongoose");
 const authorModel = require("../models/authorModel")
 const blogsModel = require("../models/blogsModel")
+const ObjectId = mongoose.Types.ObjectId
 
-const createBlog = async function(req,res){
 
-    const data = req.body
+const createBlog = async function (req, res) {
+    try {
+        const { title, body, authorId, tags, category } = req.body;
+        if (!title || !body || !authorId || !category) {
+            return res.status(400).send({ status: false, msg: "missed some required details" })
+        }
+        if (!ObjectId.isValid(authorId)) {
+            return res.status(400).send({ status: false, msg: "invalid author id" })
+        }
 
-    let authId = data.authorId
-    let checkAuthor = await authorModel.findById(authId)
-    if(!checkAuthor){
-        return res.status(400).send({status : false, msg : "author doesn't exists"})
+        let checkAuthor = await authorModel.findById(authorId)
+        if (!checkAuthor) {
+            return res.status(400).send({ status: false, msg: "author doesn't exists" })
+        }
+        const savedData = await blogsModel.create(req.body)
+        res.status(201).send({ status: true, msg: savedData })
     }
-    const savedData = await blogsModel.create(data)
-    res.status(201).send({status : true, msg : savedData})
+    catch (err) {
+        res.status(500).send({status:false,msg:err.message})
+    }
 }
 
 module.exports.createBlog = createBlog
