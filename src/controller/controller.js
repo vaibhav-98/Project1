e81431c2cbb2613022174e36715081cec6d3daee
gabhishekrao <g.abhishek.raoo@gmail.com>
2023-01-03 17:00:49 +0530

    Added

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,13 +1,25 @@
+// DELETE /blogs?queryParams
+// Delete blog documents by category, authorid, tag name, subcategory name, unpublished
+// If the blog document doesn't exist then return an HTTP status of 404 with a body like this
+const { model } = require("mongoose")
 const blogsModel = require("../models/blogsModel")
 
-const updateBlog = async function (req, res) {
-    let newData = req.body
-    let updateBlog = await blogsModel.findOneAndUpdate(
-        { _id: authorId },
-        newData,
-        { new: true })
+const deleteBlog = async function (req, res) {
 
-    res.send({ status: true, data: updateBlog })
+  let blogData = req.query
+  // let {authorId, category, tags, subcategory,isPublished } = blogData
 
+  //   if (isPublished == true)
+  //   res.send({status: false, msg: "can't delete publised blog"})
+  if (Object.keys(blogData) == 0)
+    res.send({ status: false, msg: "No data given" })
+
+    let toDelete = await blogsModel.findOneAndUpdate(blogData, { isDeleted: true }, { new: true })
+  console.log(blogData)
+  res.send({ status: true, data: toDelete })
 
 }
+
+module.exports.deleteBlog = deleteBlog
+
+
--- src/route/route.js
+++ src/route/route.js
@@ -2,6 +2,7 @@ const express=require("express");
 const router=express.Router();
 const authorController=require("../controller/authorController")
 const blogController = require("../controller/blogController")
+const toDelete = require("../controller/controller")
 
 //create author
 router.post("/authors",authorController.createAuthor)
@@ -13,6 +14,10 @@ router.post("/blogs",blogController.createBlog)
 //======================================================get API========================
 
 router.get("/blogs", blogController.getBlogsData)
+
+
+
+router.put("/blogs", toDelete.deleteBlog)
 
 
 
